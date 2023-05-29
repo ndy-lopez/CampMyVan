2f37a7b086f4fbd7eda521136cf7b13960ed6220
marc messager <marc.messager@gmail.com>
2023-05-29 17:26:27 -0400

    van model and validations created

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,2 +1,5 @@
 class Van < ApplicationRecord
+  validates :type, presence: true
+  validates :model, presence: true
+  validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_200419) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_211347) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_200419) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vans", force: :cascade do |t|
+    t.string "type"
+    t.string "model"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
