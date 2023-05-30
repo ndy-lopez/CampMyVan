95cfff526e7f28497e1065164031aa76b20a3ac7
marc messager <marc.messager@gmail.com>
2023-05-30 15:25:53 -0400

    reservations controller created

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,37 @@
+class ReservationsController < ApplicationController
+  before_action :set_reservation, only: :destroy
+  before_action :set_van, only: [:new, :create]
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    @reservation.van = @van
+    if @reservation.save
+      redirect_to van_path(@van)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    redirect_to van_path(@reservation.van), status: :see_other
+  end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def set_van
+    @van = Van.find(params[:van_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :vans do
-    resources :reservations
+    resources :reservations, except: :destroy
   end
   resources :reservations, only: :destroy
 end
