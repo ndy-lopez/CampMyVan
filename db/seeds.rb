e06dbdad0c4ab35de27d365894268098ceda4488
Andy Lopez <ndy.loredo@gmail.com>
2023-05-30 14:36:52 -0400

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,10 @@
-puts "Cleaning the db..."
 User.destroy_all
 
-puts "Creating 20 users..."
+User.create(first_name: 'John', last_name: 'Smith', email: 'john@gmail.com', password: 'password')
+User.create(first_name: 'Alicia', last_name: 'Smith', email: 'alicia@gmail.com', password: 'password')
+User.create(first_name: 'Mary', last_name: 'Smith', email: 'mary@gmail.com', password: 'password')
 
-20.times do
-  user = User.create!(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email: Faker::Internet.email,
-    password: Faker::Internet.password
-  )
-  puts "User with id #{user.id} was created"
-end
 
-puts "All done!"
+Van.create(brand: 'Volkswagen' , model: 'California', price: '60000')
+Van.create(brand: 'Mercedes' , model: 'Marco Polo', price: '100000')
+Van.create(brand: 'Nissan', model: 'NV Camper', price: '70000')
