8779d19e9b44e1df5e223677855f00f1f16b2d4e
Louis Rivest <louisrivest4@gmail.com>
2023-06-01 15:42:52 -0400

    reservation page now has cards for client reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,8 +3,8 @@ class ReservationsController < ApplicationController
   before_action :set_van, only: [:new, :show, :create]
 
   def index
-    @reservations = Reservation.where(user: current_user)
-    @van = Van.find(params[:brand])
+    @client_reservations = Reservation.where(user: current_user)
+    # @van = Van.find(params[:brand])
   end
 
   def show; end
